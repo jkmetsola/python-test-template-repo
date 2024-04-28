a8d25555dcb626c80643b22d180c299a8e4787a6
Jaakko Metsola <jkmetsola@hotmail.fi>
2024-04-28 20:55:49 +0000

    Example tests with pytest and robot

---
+++ hsl-api/tests/init_vars.py
@@ -0,0 +1,18 @@
+import os
+
+
+def obtainenv():
+    if os.path.isfile(".env"):
+        with open(".env") as f:
+            lines = [line.strip() for line in f]
+    for key, value in lines.split("=", 1):
+        if key == "HSL_API_KEY":
+            os.environ[key] = value
+
+
+def get_variables():
+    obtainenv()
+    return {
+        "HSL_GRAPHQL_API": "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql",
+        "HSL_API_KEY": os.environ["HSL_API_KEY"],
+    }
