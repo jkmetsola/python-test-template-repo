e830cc347771a63d1b623adfc5e18620ed395259
Jaakko Metsola <jkmetsola@hotmail.fi>
2024-05-01 10:14:13 +0000

    Finalize demo project and configure CI

--- customize-dev-env.sh
+++ customize-dev-env.sh
@@ -4,16 +4,24 @@ source .env
 set -u
 {
 # shellcheck disable=SC2016
-printf 'parse_git_branch() { 
+printf '
+parse_git_branch() {
   if [ -n "$(git rev-parse --git-dir 2> /dev/null)" ]; then
     echo "($(git rev-parse --abbrev-ref HEAD)) " 
   fi
 }
-export PS1="$PS1\[\033[33m\]$(parse_git_branch)\[\033[00m\]"
+run_tests() {
+  robotidy --diff --check hsl-api/tests/robot
+  robocop hsl-api/tests/robot
+  ruff check --exclude .vscode-server
+  ruff format --exclude .vscode-server --diff
+  shellcheck ./*.sh
+  hadolint Dockerfile
+}
+export PS1="$PS1\[\033[33m\]\$(parse_git_branch)\[\033[00m\]"
 '
 echo "
 git config --global user.email \"$GIT_EMAIL\"
 git config --global user.name \"$GIT_USER\"
 "
 } >> ~/.bashrc
-pip install -r requirements-dev.txt
